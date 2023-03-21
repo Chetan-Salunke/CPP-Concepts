b60b0524b0f2c31074d59b8e362c4d4d26fece10
chetan <chetansalunke019317@gmail.com>
2023-03-21 18:39:46 +0530

    primaryDatatypes

---
+++ BasicQuestions/buildindatatypes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int a = 10;
+    char b = 'b';
+    float c = 1.21;
+    double d = 1.2121;
+    bool e = true;
+
+    cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e<<endl;
+    return 0;
+}
